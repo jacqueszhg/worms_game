1cd631dcc1e25d4cc546f7e1e09f653cbce8e12f
e201823y <matthias.plichon@etu.univ-nantes.fr>
2022-01-08 21:28:14 +0100

    Probleme round fix

--- round.py
+++ round.py
@@ -9,10 +9,11 @@ class Round:
             print(GameConfig.PLAY)
         else:
             GameConfig.PLAY = 0
-            while GameConfig.LIST_WORMS[GameConfig.PLAY].is_dead(GameConfig.PLAY):
-                if GameConfig.PLAY < len(GameConfig.LIST_WORMS) - 1:
-                    GameConfig.PLAY = GameConfig.PLAY + 1
-                else:
-                    GameConfig.PLAY = 0
+        while GameConfig.LIST_WORMS[GameConfig.PLAY].is_dead(GameConfig.PLAY):
+            print(GameConfig.PLAY)
+            if GameConfig.PLAY < len(GameConfig.LIST_WORMS) - 1:
+                GameConfig.PLAY = GameConfig.PLAY + 1
+            else:
+                GameConfig.PLAY = 0
 
 Round.next_round = staticmethod(Round.next_round)
